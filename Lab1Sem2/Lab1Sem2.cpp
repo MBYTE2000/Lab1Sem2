0bf19ba9467c5afb2d830a0549af7f61c556c39e
Артём Васильевичь <megobyt2000@mail.ru>
2023-02-20 17:08:22 +0300

    Добавьте файлы проекта.

---
+++ Lab1Sem2/Lab1Sem2.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+double fun(double x, int n)
+{
+	if (n == 1) return x;
+	return x*fun(x, n - 1);
+}
+
+int main()
+{
+	double x = 0;
+	cin >> x;
+	double y = 0;
+	int n = 0;
+	cin >> n;
+	
+	if (n%2 == 0)
+	{
+		y = pow(x, n / 2) * pow(x, n / 2);
+	}
+	else
+	{
+		y = fun(x , n);
+	}
+
+	cout << "\n\n" << y << endl;
+	return 0;
+}
